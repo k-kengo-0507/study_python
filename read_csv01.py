e498cdce7678a8a1f1b20eb41ca036aaf8d22497
鎌田　謙吾 <k0507k@gmail.com>
2020-12-15 13:26:15 +0900

    修正してみました。こちらでよろしいでしょうか。 ご確認をよろしくお願いいたします。

--- read_csv01.py
+++ read_csv01.py
@@ -1,22 +1,24 @@
 import csv
 
-f = open("kimetsu.csv","r")
-reader = csv.reader(f)
-for row in reader:
-    for col in row:
-        print(col,end=",")
-
-f.close()
-
 def search():
+  with open('kimetsu.csv', mode="r") as f:
+    reader = csv.reader(f)
+    text = '' 
+    for r in reader:
+      tex = ','.join(r)
+      text += tex
+    print(text,end=",")
+
+  f.close()
+
   word=input("鬼滅の登場人物を入力してください>>>")
   
-  if word in reader:
+  if word in text:
       print("{}が見つかりました".format(word))
   else:
       print("{}が見つかりませんでした".format(word))
-      reader.append(word)
-      print("{}を追加しました".format(word))
+      # text.append(word)
+      # print("{}を追加しました".format(word))
 
 if __name__=="__main__":
   search()
