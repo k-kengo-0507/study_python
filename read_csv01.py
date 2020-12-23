f1e370a963e80d6d4355e82cdb8e523713b28f12
鎌田　謙吾 <k0507k@gmail.com>
2020-12-24 06:42:24 +0900

    ①どのように上手くいかないのか A. search関数内に入れると指示いただいたと思います。 「python 関数内に入れる」等で調べましが、理解できず先に進めない状況です。 ② どのようなコードなのか A. プルリクエストをご覧くださいませ ③どんなエラーなのか 調べた結果を踏まえながら、修正した結果、 コードを確認しようとしても、エラーも何も反応しない状況です。

--- read_csv01.py
+++ read_csv01.py
@@ -7,18 +7,17 @@ def search():
     for r in reader:
       tex = ','.join(r)
       text += tex
-    print(text,end=",")
+# if __name__=="__main__":
+  a = search()
+    # print(text,end=",")
 
-  f.close()
+  # f.close()
 
   word=input("鬼滅の登場人物を入力してください>>>")
   
-  if word in text:
+  if word in a:
       print("{}が見つかりました".format(word))
   else:
       print("{}が見つかりませんでした".format(word))
       # text.append(word)
       # print("{}を追加しました".format(word))
-
-if __name__=="__main__":
-  search()
