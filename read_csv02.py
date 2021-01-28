1c1e43d389247acf8a56f6566eb08554c45f912d
鎌田　謙吾 <k0507k@gmail.com>
2021-01-29 08:23:16 +0900

    課題1-4です。ご確認をよろしくお願いいたします。

---
+++ read_csv02.py
@@ -0,0 +1,30 @@
+import csv
+
+def search() :
+    f = open("kimetsu2.csv","w")
+
+    writer = csv.writer(f,lineterminator="\n")
+    writer.writerow(["ねづこ","たんじろう","きょうじゅうろう","ぎゆう","げんや","かなお","ぜんいつ"])
+    
+    f.close()
+
+    result = []
+    f = open("kimetsu2.csv","r")
+    reader = csv.reader(f)
+    for row in reader:
+        for col in row:
+            result.append(col)
+
+    f.close()
+
+    word=input("鬼滅の登場人物を入力してください>>>")
+
+    if word in result :
+        print("{}が見つかりました".format(word))
+    else:
+        print("{}が見つかりませんでした".format(word))
+      # text.append(word)
+        print("{}を追加しました".format(word))
+
+    # if __name__=="__main__":
+search()
